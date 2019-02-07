b73133d4237e83326628281fc3aa6e7df52fa980
LiamDuncan <duncanla@mcmaster.ca>
2019-02-06 23:20:32 -0500

    Almost finished

--- C0P1/WebServer.py
+++ C0P1/WebServer.py
@@ -1,13 +1,18 @@
 """SimpleWebServer.py: A simple web server implemented in Python 3"""
-
-__author__      = "Rong Zheng"
-__copyright__   = "Copyright 2019, McMaster University"
-
 # Import socket and relevant modules
-import socket, sys, threading,time,optparse,os
+import socket
+import sys
+import threading
+import time
+import optparse
+import os
+
+__author__ = "Rong Zheng"
+__copyright__ = "Copyright 2019, McMaster University"
 
 # Define an error message for files not found
 ErrorMsg404 = '<html><head></head><body><h1>404 Not Found</h1></body></html>'
+
 
 def validate_port(x):
     """
@@ -25,8 +30,8 @@ def validate_port(x):
             return False
     return True
 
-#A simple multi-thread  webserver
 
+# A simple multi-thread web server
 class SimpleWebServer:
     def __init__(self, port):
         threading.Thread.__init__(self)
@@ -34,17 +39,17 @@ class SimpleWebServer:
         self.port = port
         self.BUFFER_SIZE = 8192
 
-        #Create a server socket
-        self.server = socket.socket()
+        # Create a server socket
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         try:
-            self.server.bind('127.0.0.1', port)
+            self.server.bind(('127.0.0.1', port))
         except socket.error:
             print('Bind failed %s' % socket.error)
             sys.exit()
 
-        #Listen to the server socket
-        self.server.listen()    #YOUR CODE; 1 line
+        # Listen to the server socket
+        self.server.listen()
 
     def run_thread(self, conn, addr):
         # connection timeout after 60-second inactivity
@@ -55,8 +60,9 @@ class SimpleWebServer:
         while True:
             try:
                 # Receives the request message from the client
-                message =  #YOUR CODE
-                if not message: break
+                message = conn.recv(1024)
+                if not message:
+                    break
 
                 # Extract the path of the requested object from the message
                 # The path is the second part of HTTP header, identified by [1]
@@ -69,40 +75,43 @@ class SimpleWebServer:
                 # Open the local file f specified in filename for reading
                 # Because the extracted path of the HTTP request includes
                 # a character '\', we read the path from the second character
-                f = #YOUR CODE
+                # f = open(filename[1:])
+                f = open('index.html')
 
                 # Store the entire content of the requested file in a temporary buffer
-                msg = #YOUR CODE
+                msg = f.read()
 
                 # Send the HTTP response headers to the connection socket
                 # 1. HTTP version and status code
-                # YOUR CODE 1-line
+                conn.send(b'HTTP/1.1 200 OK')
 
                 # 2. Keep-Alive field
-                # YOUR CODE 1-line
+                conn.send(b'Connection: keep - alive')
 
                 # 3. Content length field
-                # YOUR CODE 1 - 3 lines
+                conn.send(b'Content-Length: ' + len(msg))
 
                 # 4. Content type field (based on the file type)
-                # YOUR CODE
+                conn.send(b'Content-Type: image/' + filename_extension[1:])
 
                 # Send the HTTP response body
                 for i in range(0, len(msg), self.BUFFER_SIZE):
                     end = min(i + self.BUFFER_SIZE, len(msg))
                     conn.send(msg[i: end])
 
-            # Exception handlieng
+            # Exception handling
             except FileNotFoundError:
                 # Send HTTP response message for file not found
+                conn.send('HTTP/1.1 404 Not Found')
+                conn.send(ErrorMsg404)
                 # YOUR CODE  1 - 3 lines
 
             except socket.timeout:
-                #Socket timeout
+                # Socket timeout
                 print("Conn socket timeout!")
                 break
             except socket.error as e:
-                #Other socket exceptions
+                # Other socket exceptions
                 print("Socket error: %s" % e)
                 break
 
@@ -111,12 +120,12 @@ class SimpleWebServer:
     def run(self):
         print('Waiting for connections on port %s' % (self.port))
         while True:
-            #Accept a new connection
+            # Accept a new connection
             try:
-                #Waiting for connection request
+                # Waiting for connection request
                 (conn, addr) = self.server.accept()
 
-                #Start a new thread to handle HTTP request/response
+                # Start a new thread to handle HTTP request/response
                 threading.Thread(target=self.run_thread, args=(conn, addr)).start()
             except:
                 break
@@ -127,6 +136,7 @@ class SimpleWebServer:
     def exit(self):
         """Close server socket"""
         self.server.close()
+
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage="%prog ServerPort")
@@ -139,6 +149,6 @@ if __name__ == '__main__':
         else:
             parser.error("ServerPort invalid!")
 
-    #Create and start the server
+    # Create and start the server
     server = SimpleWebServer(server_port)
     server.run()
